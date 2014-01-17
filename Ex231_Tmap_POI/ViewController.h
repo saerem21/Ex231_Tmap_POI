baf8746acd8bcb993010957695e4b22252183758
saerem21 <nemessisdds>
2014-01-17 14:41:28 +0900

    Initial Commit

---
+++ Ex231_Tmap_POI/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Ex231_Tmap_POI
+//
+//  Created by SDT-1 on 2014. 1. 17..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Ex231_Tmap_POITests/Ex231_Tmap_POITests.m
@@ -0,0 +1,34 @@
+//
+//  Ex231_Tmap_POITests.m
+//  Ex231_Tmap_POITests
+//
+//  Created by SDT-1 on 2014. 1. 17..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Ex231_Tmap_POITests : XCTestCase
+
+@end
+
+@implementation Ex231_Tmap_POITests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
