c79f9c791e81a73db8c83be1da00091869db082b
saerem21 <nemessisdds>
2014-01-17 15:04:54 +0900

    티맵 특정위치 지정 마커 그려주는 소스입니다.
    잘됩니다.
    이미지가 조금 묘하지만…

--- Ex231_Tmap_POI/ViewController.m
+++ Ex231_Tmap_POI/ViewController.m
@@ -7,17 +7,52 @@
 //
 
 #import "ViewController.h"
+#import "TMapView.h"
+#define TOOLBAR_HIGHT 60
+#define APP_KEY @"230febe2-ce30-303c-a8d1-827c4104161b"
+@interface ViewController ()<UISearchBarDelegate,TMapViewDelegate>
 
-@interface ViewController ()
+@property (strong,nonatomic)TMapView *mapView;
 
 @end
 
 @implementation ViewController
 
+-(void)searchBarSearchButtonClicked:(UISearchBar *)searchBar{
+    [searchBar resignFirstResponder];
+    [_mapView clearCustomObjects];
+    
+    NSString *keyword = searchBar.text;
+    TMapPathData *path = [[TMapPathData alloc] init];
+    NSArray *result = [path requestFindAddressPOI:keyword];
+    NSLog(@"number of poi %d",result.count);
+    int i =0;
+    for(TMapPOIItem *item in result){
+        NSLog(@"number %@ %@",[item getPOIName],[item getPOIPoint]);
+        NSString *markerID = [NSString stringWithFormat:@"marker %d",i++];
+        TMapMarkerItem *marker = [[TMapMarkerItem alloc] init];
+        [marker setTMapPoint:[item getPOIPoint]];
+        [marker setIcon:[UIImage imageNamed:@"ball1.png"]];
+        [marker setCanShowCallout:YES];
+        [marker setCalloutTitle:[item getPOIName]];
+        [marker setCalloutSubtitle:[item getPOIAddress]];
+        
+        [_mapView addCustomObject:marker ID:markerID];
+    }
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    CGRect rect = CGRectMake(0, TOOLBAR_HIGHT, 320, 300);
+    _mapView = [[TMapView alloc] initWithFrame:rect];
+    [_mapView setSKPMapApiKey:APP_KEY];
+    _mapView.zoomLevel = 12.0;
+    _mapView.delegate =self;
+    [self.view addSubview:_mapView];
+
+    
 }
 
 - (void)didReceiveMemoryWarning
